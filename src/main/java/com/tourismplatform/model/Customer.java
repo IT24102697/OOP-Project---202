7f899304c9018db9d8e4bf47c01f6b3f9a2e1a62
IT24102697 <it24102697@my.sliit.lk>
2025-05-22 16:59:42 +0530

    Create Customer.java

---
+++ src/main/java/com/tourismplatform/model/Customer.java
@@ -0,0 +1,12 @@
+package com.tourismplatform.model;
+
+public class Customer extends User {
+    public Customer(String username, String password) {
+        super(username, password);
+    }
+
+    @Override
+    public String getRole() {
+        return "customer";
+    }
+}
